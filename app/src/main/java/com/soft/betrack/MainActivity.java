32eb574e08beeca559d170367c5c998ace443f7e
abdoulrazak151 <abdoulrazakmahamadou151@gmail.com>
2022-05-20 17:37:15 +0100

    cor 1

--- app/src/main/java/com/soft/betrack/MainActivity.java
+++ app/src/main/java/com/soft/betrack/MainActivity.java
@@ -1,23 +1,32 @@
 package com.soft.betrack;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.navigation.NavigationView;
 import com.soft.betrack.localiser_mon_troupeau.MapsFragment;
 import com.soft.betrack.localiser_mon_troupeau.MapsLarge;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    MaterialToolbar toolbar;
    FragmentManager fragmentManager;
    FragmentTransaction fragmentTransaction;
     MaterialCardView maPositionCard, monTroupeauCard, zonePaturageCard, pointEauCard;
     MapsFragment mapsFragment;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +36,17 @@ public class MainActivity extends AppCompatActivity {
         monTroupeauCard=findViewById(R.id.mon_troupeau);
         zonePaturageCard=findViewById(R.id.zone_paturage);
         pointEauCard=findViewById(R.id.point_eau);
+        toolbar=findViewById(R.id.toolbar);
         mapsFragment=new MapsFragment();
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
 
 
@@ -55,5 +74,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
